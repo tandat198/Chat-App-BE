2916c13962d4c216928d122898f490ec24ad940f
Dat Nguyen <tandat198@gmail.com>
2020-04-13 22:57:03 +0700

    update

--- server.js
+++ server.js
@@ -2,12 +2,30 @@ const express = require("express");
 const cors = require("cors");
 const mongoose = require("mongoose");
 const port = process.env.PORT || 5000;
-
+//
 const app = express();
+// app.use((req, res, next) => {
+//     res.setHeader("Access-Control-Allow-Origin", "*");
+//     res.setHeader("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
+//     res.setHeader("Access-Control-Allow-Methods", "GET, POST, PATCH, DELETE");
+//     next();
+// });
+app.use(cors());
+
+const server = require("http").Server(app);
+
+const io = require("socket.io")(server);
+
+io.on("connection", function (socket) {
+    socket.emit("news", { hello: "world" });
+    socket.on("my other event", function (data) {
+        console.log(data);
+    });
+});
 
 app.use(express.json());
 
-app.use("/api", cors(), require("./routes/api"));
+app.use("/api", require("./routes/api"));
 
 mongoose.connect(
     `mongodb+srv://datng198:Dat12345678@cluster0-xejqn.mongodb.net/test?retryWrites=true&w=majority`,
