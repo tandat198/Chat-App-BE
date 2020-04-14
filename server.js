930451cc9aa111195b8a63c2194d2287f1f6da04
Dat Nguyen <tandat198@gmail.com>
2020-04-14 17:22:22 +0700

    update

--- server.js
+++ server.js
@@ -32,12 +32,12 @@ io.on("connection", function (socket) {
     socket.on("room", async function (data) {
         socket.join(data.room._id);
         if (data.msg && data.room._id) {
-            io.to(data.room._id).emit("sendMsgFromServer", `msg from server to room ${data.room._id}: ${data.msg} by ${data.user.name}`);
             const message = new Message({
                 senderId: data.user._id,
                 senderName: data.user.name,
                 text: data.msg
             });
+            io.to(data.room._id).emit("sendMsgFromServer", message);
             const groupMessage = await GroupMessage.findOne({ groupId: data.room._id });
             groupMessage.messages.push(message);
             groupMessage.save();
