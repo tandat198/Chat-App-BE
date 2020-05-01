38ad8af5feeba8cd5b3299c92e3ba931863d07b3
Dat Nguyen <tandat198@gmail.com>
2020-05-01 07:10:23 +0700

    update

--- server.js
+++ server.js
@@ -1,25 +1,27 @@
 const express = require("express");
-const cors = require("cors");
 const mongoose = require("mongoose");
-const port = process.env.PORT || 5000;
-const { Message } = require("./models/Message");
-const GroupMessage = require("./models/GroupMessage");
-const app = express();
 const http = require('http');
+const port = process.env.PORT || 5000;
+
+const app = express();
 const server = http.createServer(app);
 const socketIO = require('socket.io')
+const { Message } = require("./models/Message");
+const GroupMessage = require("./models/GroupMessage");
 const io = socketIO(server)
+
 const { mongoURI } = require('./config')
-// app.use((req, res, next) => {
-//     res.setHeader("Access-Control-Allow-Origin", "*");
-//     res.setHeader("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
-//     res.setHeader("Access-Control-Allow-Methods", "GET, POST, PATCH, DELETE");
-//     next();
-// });
+
+app.use((req, res, next) => {
+    res.setHeader("Access-Control-Allow-Origin", "https://fastchat.netlify.app");
+    res.setHeader("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
+    res.setHeader("Access-Control-Allow-Methods", "GET, POST, PATCH, DELETE");
+    next();
+});
 
 app.use(express.json());
 
-app.use("/api", cors(), require("./routes/api"));
+app.use("/api", require("./routes/api"));
 
 mongoose.connect(
     mongoURI,
@@ -49,3 +51,5 @@ io.on("connection", function (socket) {
 });
 
 server.listen(port, () => console.log(`Server is running on port ${port}`));
+
+module.exports = server
