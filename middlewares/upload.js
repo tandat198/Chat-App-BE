a0a45569be5dde74062abffb668526fc1a57e764
Dat Nguyen <51848355+tandat198@users.noreply.github.com>
2020-06-21 22:16:14 +0700

    Update upload.js

--- middlewares/upload.js
+++ middlewares/upload.js
@@ -2,6 +2,7 @@ const aws = require("aws-sdk");
 const multer = require("multer");
 const multerS3 = require("multer-s3");
 const path = require('path')
+const { v4: uuidv4 } = require('uuid');
 const { bucket, accessKeyId, secretAccessKey } = require('../config')
 
 const s3 = new aws.S3({
@@ -18,7 +19,10 @@ const upload = multer({
             cb(null, { fieldName: `${file.originalname}` });
         },
         key: function (req, file, cb) {
-            cb(null, `${Date.now().toString()}${file.originalname}`);
+            const splitOfFileName = file.originalname.split(".");
+            const fileType = splitOfFileName[splitOfFileName.length - 1];
+            const newFileName = `${uuidv4()}.${fileType}`;
+            cb(null, newFileName);
         }
     }),
     limits: {
