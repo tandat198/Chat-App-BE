13b668e19290bc8c17d104db0679e0fe2fb56768
tandat198 <tandat198@gmail.com>
2020-07-05 10:40:07 +0700

    update

--- middlewares/upload.js
+++ middlewares/upload.js
@@ -1,13 +1,13 @@
 const aws = require("aws-sdk");
 const multer = require("multer");
 const multerS3 = require("multer-s3");
-const path = require('path')
-const { v4: uuidv4 } = require('uuid');
-const { bucket, accessKeyId, secretAccessKey } = require('../config')
+const path = require("path");
+const { v4: uuidv4 } = require("uuid");
+const { bucket, accessKeyId, secretAccessKey } = require("../config");
 
 const s3 = new aws.S3({
     accessKeyId,
-    secretAccessKey
+    secretAccessKey,
 });
 
 const upload = multer({
@@ -16,36 +16,34 @@ const upload = multer({
         bucket,
         acl: "public-read",
         metadata: function (req, file, cb) {
-            cb(null, { fieldName: `${file.originalname}` });
+            cb(null, { fieldName: file.originalname });
         },
         key: function (req, file, cb) {
-            const splitOfFileName = file.originalname.split(".");
-            const fileType = splitOfFileName[splitOfFileName.length - 1];
-            const newFileName = `${uuidv4()}.${fileType}`;
-            cb(null, newFileName);
-        }
+            const fileAfterSplited = file.originalname.split(".");
+            cb(null, `images/${uuidv4()}.${fileAfterSplited[fileAfterSplited.length - 1]}`);
+        },
     }),
     limits: {
-        fileSize: 3 * Math.pow(1024, 2)
+        fileSize: 3 * Math.pow(1024, 2),
     },
     fileFilter: function (req, file, callback) {
         var ext = path.extname(file.originalname);
-        if (ext !== '.png' && ext !== '.jpg' && ext !== '.jpeg') {
-            return callback(new Error('Only images are allowed'))
+        if (ext !== ".png" && ext !== ".jpg" && ext !== ".jpeg") {
+            return callback(new Error("Only images are allowed"));
         }
         return callback(null, true);
-    }
+    },
 });
 
 const uploadSingle = (type, req, res) => {
     return upload.single(type)(req, res, function (err) {
         if (err instanceof multer.MulterError) {
-            ["name", "storageErrors"].forEach(e => delete err[e]);
+            [("name", "storageErrors")].forEach((e) => delete err[e]);
             return res.status(400).json({ error: err });
         } else if (err) {
+            console.log(err);
             return res.status(400).json({ error: err });
         } else {
-            console.log(req.file)
             return res.status(200).json({ linkUrl: req.file.location });
         }
     });
@@ -56,5 +54,5 @@ const uploadSingleImage = (req, res) => {
 };
 
 module.exports = {
-    uploadSingleImage
+    uploadSingleImage,
 };
--- routes/api/Post/index.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const postController = require("./controller");
-const { authenticate } = require("../../../middlewares/auth");
-
-router.post("/", authenticate, postController.postNewPost);
-
-module.exports = router;
